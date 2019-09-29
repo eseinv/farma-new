0f95769d2dcf83f8f8f18866ae89ce01978872e4
Ievgen Sein <eseinv@gmail.com>
2019-09-29 11:01:49 +0300

    init

---
+++ src/navbar/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { NavBrand } from "./nav-brand";
+
+export const NavBar = () => <NavBrand />;
---
+++ src/navbar/nav-brand.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FpLogo } from "./style";
+
+const NavBrand = () => (
+  <FpLogo
+    className="img-fluid"
+    src="https://api.farmapalatia.gr/images/fp-logo-2x.png"
+    alt="fp-logo-brand"
+  />
+);
+
+export { NavBrand };
